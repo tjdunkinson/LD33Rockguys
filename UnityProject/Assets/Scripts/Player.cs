6b95654bc7817f6ff8fa817bfcfaf0b57f3a05ce
Travers Dunkinson <tjdunkinson@gmail.com>
2015-08-23 12:57:15 +1000

    Added project files

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float playerNum;
+	public float baseSpeed;
+	public GameObject ball;
+
+	Vector3 direction;
+	public float speed;
+	bool canGrab;
+
+
+	CharacterController cont;
+	// Use this for initialization
+	void Start () {
+
+		cont = GetComponent<CharacterController> ();
+
+		speed = baseSpeed;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	
+		if (Input.GetButtonDown("GrabButton"+playerNum) && canGrab)
+		{
+			ball.SendMessage("Grabbed", this.gameObject);
+			direction = new Vector3(Input.GetAxis("Xaxis"+playerNum),0,0);
+
+		}
+		else
+		{
+			direction = new Vector3(Input.GetAxis("Xaxis"+playerNum),Input.GetAxis("Yaxis"+playerNum),0);
+		}
+		if (Input.GetButtonUp("GrabButton"+playerNum) && canGrab)
+		{
+			ball.SendMessage("Ungrabbed", this.gameObject);
+		}
+
+		if (Input.GetAxis("Xaxis"+playerNum) != 0 || Input.GetAxis("Yaxis"+playerNum) != 0)
+		{
+			direction = direction.normalized;
+			
+			cont.Move((direction * speed) * Time.deltaTime);
+		}
+
+
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		canGrab = true;
+	}
+	void OnTriggerExit (Collider col)
+	{
+		canGrab = false;
+
+		speed = baseSpeed;
+	}
+
+	void ModifySpeed (float newSpeed)
+	{
+		speed = baseSpeed / newSpeed;
+	}
+}
---
+++ UnityProject/Assets/Scripts/ball.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+
+	public int holdignMe;
+
+	public GameObject[] players; // 0 is prime player
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (holdignMe > 0)
+		{
+			players[0].SendMessage("ModifySpeed", 2 * holdignMe);
+			Vector3 slide = Vector3.zero;
+			slide.x = players[0].transform.position.x;
+			transform.position = slide;
+
+			//print (players[0].name);
+		
+		}
+	
+	}
+
+	void Grabbed (GameObject playerGrab)
+	{
+		holdignMe ++;
+		players [holdignMe - 1] = playerGrab;
+	}
+	void Ungrabbed (GameObject playerGrab)
+	{
+		holdignMe --;
+	}
+}
