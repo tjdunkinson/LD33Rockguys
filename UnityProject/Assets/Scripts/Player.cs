254aef4b78cc975f7d675cd16f1a558502b8a33c
Travers Dunkinson <tjdunkinson@gmail.com>
2015-08-23 20:43:24 +1000

    Just a fuck ton of work getting the game running

--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -1,47 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
+public class playerDetails
+{
+	public GameObject playerObject;
+	public bool gotPowerUp;
+	public enum TeamCol
+	{
+		Red,
+		Blue
+	};
 
+	public TeamCol teamCol;
+
+
+}
 public class Player : MonoBehaviour {
 
 	public float playerNum;
 	public float baseSpeed;
 	public GameObject ball;
+	public bool powerUp = false;
+	public GameObject puManager;
+
+	public enum TeamColour
+	{
+		Red,
+		Blue
+	};
+
+	public TeamColour team;
+	
 
 	Vector3 direction;
 	public float speed;
 	bool canGrab;
-
+	bool latched = false;
+	float puTimer;
+	
 
 	CharacterController cont;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 
 		cont = GetComponent<CharacterController> ();
-
 		speed = baseSpeed;
 
-	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 
-
-
-	
 		if (Input.GetButtonDown("GrabButton"+playerNum) && canGrab)
 		{
-			ball.SendMessage("Grabbed", this.gameObject);
-			direction = new Vector3(Input.GetAxis("Xaxis"+playerNum),0,0);
+			var sendDets = new playerDetails();
+			sendDets.gotPowerUp = powerUp;
+			sendDets.playerObject = this.gameObject;
+			sendDets.teamCol = team;
+
+			ball.SendMessage("Grabbed", sendDets);
+			//ball.SendMessage("Grabbed", powerUp);
+			latched = true;
 
 		}
-		else
-		{
-			direction = new Vector3(Input.GetAxis("Xaxis"+playerNum),Input.GetAxis("Yaxis"+playerNum),0);
-		}
-		if (Input.GetButtonUp("GrabButton"+playerNum) && canGrab)
+		if (Input.GetButtonUp("GrabButton"+playerNum) && latched)
 		{
 			ball.SendMessage("Ungrabbed", this.gameObject);
+			latched = false;
 		}
 
 		if (Input.GetAxis("Xaxis"+playerNum) != 0 || Input.GetAxis("Yaxis"+playerNum) != 0)
@@ -52,21 +78,68 @@ public class Player : MonoBehaviour {
 		}
 
 
+		if (latched)
+		{
+			direction = new Vector3(Input.GetAxis("Xaxis"+playerNum),0,0);
+		}
+		else
+		{
+			direction = new Vector3(Input.GetAxis("Xaxis"+playerNum),Input.GetAxis("Yaxis"+playerNum),0);
+			
+			//print ("freedom");
+			speed = baseSpeed;
+		}
+
+		if (puTimer > 0)
+		{
+			puTimer -= Time.deltaTime;
+			speed =  baseSpeed * 2;
+		}
+		if (puTimer < 0)
+		{
+			DeactivatePower();
+		}
+
 	}
 
 	void OnTriggerEnter (Collider col)
 	{
-		canGrab = true;
+		if (col.tag == "Ball") 
+		{
+			canGrab = true;
+		}
+		if (col.tag == "PowerUp")
+		{
+			ActivatePower(col.gameObject);
+		}
+
 	}
 	void OnTriggerExit (Collider col)
 	{
-		canGrab = false;
-
-		speed = baseSpeed;
+		if (col.tag == "Ball") 
+		{
+			canGrab = false;
+			latched = false;
+		}
 	}
 
 	void ModifySpeed (float newSpeed)
 	{
 		speed = baseSpeed / newSpeed;
 	}
+
+	void ActivatePower (GameObject puObject)
+	{
+		powerUp = true;
+		puManager.SendMessage("Collected");
+		puTimer = 5f;
+		Destroy (puObject, 0.2f);
+	}
+	void DeactivatePower ()
+	{
+		powerUp = false;
+		speed = baseSpeed;
+	}
 }
+
+
---
+++ UnityProject/Assets/Scripts/PowerUpManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpManager : MonoBehaviour {
+
+	public GameObject powerUpObject;
+
+	public Transform[] spawnPoints;
+
+	public float timer;
+
+
+	GameObject activatePowerup;
+	// Use this for initialization
+	void Start () {
+
+		//spawnPoints = GetComponentsInChildren<Transform> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (timer < 0 && activatePowerup == null)
+		{
+			SpawnPowerUp();
+			//timer =  10f;
+		}
+
+		timer -= Time.deltaTime;
+	
+	}
+
+	void SpawnPowerUp ()
+	{
+		if (activatePowerup == null)
+		{
+			int spawnNum = Random.Range (0, spawnPoints.Length);
+			activatePowerup = Instantiate (powerUpObject, spawnPoints[spawnNum].position, spawnPoints[spawnNum].rotation) as GameObject;
+		}
+	}
+	void Collected ()
+	{
+		activatePowerup = null;
+		timer =  5f;
+	}
+}
