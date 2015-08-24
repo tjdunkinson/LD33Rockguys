83626f60f41f1789be0bad872cc408d743c00209
Travers Dunkinson <tjdunkinson@gmail.com>
2015-08-24 14:29:14 +1000

    finished off core gameplay

--- UnityProject/Assets/Scripts/PowerUpManager.cs
+++ UnityProject/Assets/Scripts/PowerUpManager.cs
@@ -4,18 +4,23 @@ using System.Collections;
 public class PowerUpManager : MonoBehaviour {
 
 	public GameObject powerUpObject;
-
-	public Transform[] spawnPoints;
-
+	public Transform[] topSpawnPoints;
+	public Transform[] botSpawnPoints;
 	public float timer;
+	public GameObject redPlayer;
+	public GameObject bluePlayer;
 
-
+	GameObject ball;
+	Transform chosenSpawn;
+	public float[] smallestFinder;
 	GameObject activatePowerup;
+	int spawnNum;
 	// Use this for initialization
 	void Start () {
 
 		//spawnPoints = GetComponentsInChildren<Transform> ();
-	
+
+		ball = GameObject.FindGameObjectWithTag ("Ball");
 	}
 	
 	// Update is called once per frame
@@ -33,11 +38,69 @@ public class PowerUpManager : MonoBehaviour {
 
 	void SpawnPowerUp ()
 	{
-		if (activatePowerup == null)
+		//int topOrBot = Random.Range (0, 2);
+
+		for (int i = 0; i < topSpawnPoints.Length; i++) 
 		{
-			int spawnNum = Random.Range (0, spawnPoints.Length);
-			activatePowerup = Instantiate (powerUpObject, spawnPoints[spawnNum].position, spawnPoints[spawnNum].rotation) as GameObject;
+			smallestFinder [i] = topSpawnPoints [i].position.x - ball.transform.position.x;
+			if (smallestFinder [i] < 0) 
+			{
+				smallestFinder [i] = -smallestFinder [i];
+			}
 		}
+		for (int i = 0; i < smallestFinder.Length; i++)
+		{
+			if (smallestFinder [i] == Mathf.Min(smallestFinder))
+			{
+				spawnNum = i;
+			}
+		}
+
+		if (spawnNum == 0)
+		{
+			if (redPlayer.transform.position.y > 0)
+			{
+				activatePowerup = Instantiate (powerUpObject, topSpawnPoints[spawnNum].position, topSpawnPoints[spawnNum].rotation) as GameObject;
+			}
+			else
+			{
+				activatePowerup = Instantiate (powerUpObject, botSpawnPoints[spawnNum].position, botSpawnPoints[spawnNum].rotation) as GameObject;
+			}
+		}
+		if (spawnNum == 1)
+		{
+			int topOrBot = Random.Range (0, 2);
+
+			if (topOrBot == 0)
+			{
+				activatePowerup = Instantiate (powerUpObject, topSpawnPoints[spawnNum].position, topSpawnPoints[spawnNum].rotation) as GameObject;
+			}
+			else
+			{
+				activatePowerup = Instantiate (powerUpObject, botSpawnPoints[spawnNum].position, botSpawnPoints[spawnNum].rotation) as GameObject;
+			}
+			
+		}
+		if (spawnNum == 2)
+		{
+			if (bluePlayer.transform.position.y > 0)
+			{
+				activatePowerup = Instantiate (powerUpObject, topSpawnPoints[spawnNum].position, topSpawnPoints[spawnNum].rotation) as GameObject;
+			}
+			else
+			{
+				activatePowerup = Instantiate (powerUpObject, botSpawnPoints[spawnNum].position, botSpawnPoints[spawnNum].rotation) as GameObject;
+			}
+			
+		}
+
+
+		//activatePowerup = Instantiate (powerUpObject, topSpawnPoints[spawnNum].position, topSpawnPoints[spawnNum].rotation) as GameObject;
+		/*if (activatePowerup == null)
+		{
+			//int spawnNum = Random.Range (0, botSpawnPoints.Length);
+			activatePowerup = Instantiate (powerUpObject, topSpawnPoints[spawnNum].position, topSpawnPoints[spawnNum].rotation) as GameObject;
+		}*/
 	}
 	void Collected ()
 	{
